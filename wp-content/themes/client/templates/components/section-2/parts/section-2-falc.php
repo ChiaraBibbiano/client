247367592a6d3a1cd8af57752b52b28747613f13
ChiaraBibbiano <chiara.bibbiano@student.hepl.be>
2026-04-21 12:57:05 +0200

    feat: implement section 2

--- wp-content/themes/client/templates/components/section-2/parts/section-2-falc.php
+++ wp-content/themes/client/templates/components/section-2/parts/section-2-falc.php
@@ -1,1 +1,24 @@
 <?php
+$title = get_field('title_two_falc');
+$subtitle = get_field('subtitle_two_falc');
+$title_cards = get_field('cards_title');
+$cards = get_field('cards_falc');
+?>
+
+<section class="section-2-falc">
+    <h2 class="section-2-falc__title"><?= $title ?></h2>
+    <p class="section-2-falc__subtitle"><?= $subtitle ?></p>
+    <div class="section-2-falc__container">
+        <h3 class="section-2-falc__title_cards"> <?= $title_cards ?> </h3>
+        <?php foreach ($cards as $card): ?>
+            <div class="section-2-falc__cards">
+                <p class="section-2-falc__card-text">
+                    <?= $card['card'] ?>
+                </p>
+                <p class="section-1-falc__card-number">
+                    <?= $card['number_card'] ?>
+                </p>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</section>
--- wp-content/themes/client/templates/components/section-2/parts/section-2.php
+++ wp-content/themes/client/templates/components/section-2/parts/section-2.php
@@ -1,1 +1,23 @@
 <?php
+$title = get_field('title_two');
+$subtitle = get_field('subtitle_two');
+$cards = get_field('cards_two');
+?>
+
+<section class="section-2">
+    <h2 class="section-2__title"><?= $title ?></h2>
+    <p class="section-2__subtitle"><?= $subtitle ?></p>
+    <div class="section-2__container">
+        <?php foreach ($cards as $card): ?>
+            <div class="section-2__card">
+                <p class="section-2__card-text">
+                    <?= $card['card'] ?>
+                </p>
+                <p class="section-2__card-number">
+                    <?= $card['number_card'] ?>
+                </p>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</section>
+
